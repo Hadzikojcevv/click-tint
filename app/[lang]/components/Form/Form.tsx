6b0cf4a86a348508acd1e33fa654c187a8fa08dd
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2023-12-14 02:49:34 +0100

    mail refs

--- app/[lang]/components/Form/Form.tsx
+++ app/[lang]/components/Form/Form.tsx
@@ -1,26 +1,97 @@
-import React from 'react'
+'use client'
+import React, { useRef, useState } from 'react'
 
 type FormProps = {
-    lang: any
+  lang: any
 }
 
-const Form = ({lang}: FormProps) => {
+type FormDataType = {
+  name: string
+  email: string
+  tel: string | number
+  location: string
+  company?: string
+  message?: string
+}
+
+const Form = ({ lang }: FormProps) => {
+  const [formData, setFormData] = useState<FormDataType>()
+
+  const nameInput = useRef<HTMLInputElement>(null)
+  const emailInput = useRef<HTMLInputElement>(null)
+  const telInput = useRef<HTMLInputElement>(null)
+  const locationInput = useRef<HTMLInputElement>(null)
+  const companyInput = useRef<HTMLInputElement>(null)
+  const messageInput = useRef<HTMLTextAreaElement>(null)
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+
+    setFormData({
+      name: nameInput.current!.value,
+      email: emailInput.current!.value,
+      tel: telInput.current!.value,
+      location: locationInput.current!.value,
+      company: companyInput.current!.value,
+      message: messageInput.current!.value
+    })
+  }
+
   return (
-    <form className='flex basis-1/2 flex-col gap-3 mt-6 lg:mt-0'>
-      <input type='text' className=' border-thin p-2 rounded-sm outline-none' placeholder={lang.home.form?.inputs?.name ?? "Name"} />
-      <div className='flex flex-col lg:flex-row justify-between gap-4'>
-        <input type='email' className='basis-1/2  border-thin p-2 rounded-sm outline-none' placeholder={lang.home.form?.inputs?.email ?? "E-mail"} />
-        <input type='tel' className='basis-1/2  border-thin p-2 rounded-sm outline-none' placeholder={lang.home.form?.inputs?.tel ?? "Telephone"} />
+    <form
+      className='mt-6 flex basis-1/2 flex-col gap-3 lg:mt-0'
+      onSubmit={handleSubmit}
+    >
+      <input
+        type='text'
+        className=' border-thin rounded-sm p-2 outline-none'
+        placeholder={lang.home.form?.inputs?.name ?? 'Name'}
+        ref={nameInput}
+      />
+      <div className='flex flex-col justify-between gap-4 lg:flex-row'>
+        <input
+          type='email'
+          className='border-thin  basis-1/2 rounded-sm p-2 outline-none'
+          placeholder={lang.home.form?.inputs?.email ?? 'E-mail'}
+          ref={emailInput}
+        />
+        <input
+          type='tel'
+          className='border-thin  basis-1/2 rounded-sm p-2 outline-none'
+          placeholder={lang.home.form?.inputs?.tel ?? 'Telephone'}
+          ref={telInput}
+        />
       </div>
 
-      <div className='flex flex-col lg:flex-row justify-between gap-4'>
-        <input type='text' className='basis-1/2  border-thin p-2 rounded-sm outline-none' placeholder={lang.home.form?.inputs?.location ?? "Location"} />
-        <input type='text' className='basis-1/2  border-thin p-2 rounded-sm outline-none' placeholder={lang.home.form?.inputs?.company ?? "Company Name"} />
+      <div className='flex flex-col justify-between gap-4 lg:flex-row'>
+        <input
+          type='text'
+          className='border-thin  basis-1/2 rounded-sm p-2 outline-none'
+          placeholder={lang.home.form?.inputs?.location ?? 'Location'}
+          ref={locationInput}
+        />
+        <input
+          type='text'
+          className='border-thin  basis-1/2 rounded-sm p-2 outline-none'
+          placeholder={lang.home.form?.inputs?.company ?? 'Company Name'}
+          ref={companyInput}
+        />
       </div>
 
-      <textarea name='' id='' cols={30} rows={10} placeholder={lang.home.form?.inputs?.message ?? "Your Message"} className='border-thin p-2 rounded-sm outline-none'></textarea>
-      <button type='submit' className='bg-primary w-full p-3 rounded-sm outline-none text-white font-semibold'>
-      {lang.home.form?.inputs?.btn ?? "Ask For Price..."}
+      <textarea
+        name=''
+        id=''
+        cols={30}
+        rows={10}
+        placeholder={lang.home.form?.inputs?.message ?? 'Your Message'}
+        className='border-thin rounded-sm p-2 outline-none'
+        ref={messageInput}
+      ></textarea>
+      <button
+        type='submit'
+        className='bg-primary w-full rounded-sm p-3 font-semibold text-white outline-none'
+      >
+        {lang.home.form?.inputs?.btn ?? 'Ask For Price...'}
       </button>
     </form>
   )
