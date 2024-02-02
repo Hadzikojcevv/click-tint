f2c60daa700f1332e3e7abccd3d488caac06ad90
Slobodan Krdzev <slobodankrdzev@yahoo.com>
2024-02-02 16:44:54 +0100

    form corected

--- app/[lang]/components/Form/Form.tsx
+++ app/[lang]/components/Form/Form.tsx
@@ -35,10 +35,7 @@ class Contact {
 
 const Form = ({ lang }: FormProps) => {
 
-const clickTint = "mnqejpyw"
-
-
-  const [state, handleSubmit] = useForm('mrgwnngw')
+  const [state, handleSubmit] = useForm("mnqejpyw")
   const form = useRef<HTMLFormElement>(null)
   const [phoneNum, setPhoneNum] = useState('')
 
