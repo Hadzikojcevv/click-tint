758a853db72824db60c32447c58d44805612675a
Slobodan-Krdzev <slobodankrdzev@yahoo.com>
2024-08-08 13:20:25 +0200

    fix btn

--- app/[lang]/components/Shared/AskButton.tsx
+++ app/[lang]/components/Shared/AskButton.tsx
@@ -2,31 +2,57 @@
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import MainCta from './MainCta'
+import { usePathname } from 'next/navigation'
 
 type AskButtonProps = {
-    page:any
+  page: any
 }
 
-const AskButton = ({page}: AskButtonProps) => {
+function scrollToSectionWithOffset(hash: string) {
+  const targetElement = document.querySelector(hash);
+  if (targetElement) {
+    const offset = 100; // Adjust this to the height of your fixed navigation bar
+    const elementPosition = targetElement.getBoundingClientRect().top + window.pageYOffset;
+    const offsetPosition = elementPosition - offset;
 
-    const [isVisible, setIsVisible] = useState(true)
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth',
+    });
+  }
+}
 
-    useEffect(() => {
-        const handleScroll = () => {
-            
-          const scrollY = window.scrollY || document.documentElement.scrollTop;
-          const triggerScrollPosition = 6000;
-    
-          setIsVisible(scrollY < triggerScrollPosition);
-        };
-    
-        window.addEventListener('scroll', handleScroll);
-    
-        return () => {
-          window.removeEventListener('scroll', handleScroll);
-        };
-      }, []);
+const AskButton = ({ page }: AskButtonProps) => {
+  const [isVisible, setIsVisible] = useState(true)
+  const pathname = usePathname()
 
+  useEffect(() => {
+    
+    const handleScroll = () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop
+      const triggerScrollPosition = 6000
+
+      setIsVisible(scrollY < triggerScrollPosition)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (pathname.includes('#')) {
+      scrollToSectionWithOffset(pathname.split('#')[1]);
+    }
+  }, [pathname]);
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    scrollToSectionWithOffset('#mainForm');
+  };
+  
 
   return (
     <Link
@@ -34,6 +60,7 @@ const AskButton = ({page}: AskButtonProps) => {
       className={`${isVisible ? 'fixed' : 'hidden'}  bottom-3 right-3 z-50`}
       type='button'
       aria-label='Go To Ask For Price Section'
+      onClick={handleClick}
     >
       <MainCta text={page.footer.btn ?? 'Ask For Price!'} />
     </Link>
